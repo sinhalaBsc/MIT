1f93ac737d2f8bebf2ffe3018d6d4ae72179aae9
sinhalaBsc <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-11 14:52:27 +0300

    main binary search

---
+++ binarySearch.py
@@ -0,0 +1,13 @@
+
+import pybs
+
+arr=[1,3,4,6,7,8,9]       # array whitch we input
+
+bs=pybs.binarySearch()    # define the ' binary search ' class
+pr=bs.binarySearch(6,arr) # search ...
+
+print(pr)                 # print search result
+
+
+           #               attention  !!!!!
+           # do not input value whitch not in the array
